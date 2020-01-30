418347d2950ebd5a0c74d4f97aa68f44b021b25e
Anwar Hsu <ahsu@ucsd.edu>
2020-01-29 17:32:36 -0800

    another one

--- lab 1 code/Message.ino
+++
@@ -1,9 +0,0 @@
-void setupMessage(){
-  // Serial begin at 9600 
-  Serial.begin(9600);
-}
-
-void printTime(int integer_to_print){
-  // Serial print integer_to_print
-  Serial.println(integer_to_print);
-}
